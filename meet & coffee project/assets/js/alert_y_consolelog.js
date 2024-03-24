34e440f6567463161e64fb33de7a0769761ca645
Rodrigo ICM <rodrigoc.merino@gmail.com>
2024-03-24 19:04:30 -0300

    ver.1

---
+++ meet & coffee project/assets/js/alert_y_consolelog.js
@@ -0,0 +1,2 @@
+ alert("Estoy ejecutando la función alert");
+ console.log("Estoy ejecutando la función console.log"); 
---
+++ meet & coffee project/assets/js/ejercicio_algebra.js
@@ -0,0 +1,8 @@
+3 + 4
+x = 3 + 4
+y = x
+y = y+y
+z = "Hola"
+z = z + y
+console.log(z);
+alert(z);
